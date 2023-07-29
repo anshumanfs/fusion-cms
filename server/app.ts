00f38f3c57d8e1c734e538355f3631efec40d177
Anshuman Nayak <64569737+anshuman61@users.noreply.github.com>
2023-07-29 16:07:43 +0530

    Configured with next js

---
+++ server/app.ts
@@ -0,0 +1,18 @@
+import Express, { Request, Response } from 'express';
+import { createServer } from 'http';
+import next from 'next';
+
+const port = parseInt(process.env.PORT || '3000', 10);
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+    const server = Express();
+
+    server.all('*', (req: Request, res: Response) => handle(req, res));
+
+    createServer(server).listen(port, () => {
+        console.log(`> Ready on http://localhost:${port}`);
+    });
+})
