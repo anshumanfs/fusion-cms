6c2664ac546a6bcfa9ccb3f630be53a483b774af
Anshuman Nayak <64569737+anshuman61@users.noreply.github.com>
2023-07-29 21:16:36 +0530

    Adding up shadcn components

---
+++ server/connectors/mongo.ts
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+
+// write an interface for mongoose createConnection options
+interface MongooseConfigOptions {
+    bufferCommands: boolean;
+    autoIndex: boolean;
+    dbName: string;
+    user: string;
+    pass: string;
+    maxPoolSize: number;
+    minPoolSize: number;
+    socketTimeoutMS: number;
+    family: number;
+    authSource: string;
+    serverSelectionTimeoutMS: number;
+    heartbeatFrequencyMS: number;
+}
+
+interface MongooseConfig {
+    uri: string;
+    options: Partial<MongooseConfigOptions>;
+}
+
+
+export default function connect(config: MongooseConfig) {
+    const { uri, options } = config;
+    const conn = mongoose.createConnection(uri, options);
+    return conn;
+}
