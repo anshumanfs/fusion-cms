bf30e906e86fa6b3f5c937481685c7fd2f950681
Anshuman Nayak <anshuman.nayak@zeno.health>
2025-02-20 23:40:33 +0530

    33- Temp sidebar changes

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,23 +1,75 @@
-'use client';
-import React from 'react';
-import { SideBar } from './sideBar';
-import { ModeToggle } from '@/components/themeToggle';
-export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+// 'use client';
+// import React from 'react';
+// import { SideBar } from './sideBar';
+// import { ModeToggle } from '@/components/themeToggle';
+// export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+//   return (
+//     <>
+//       <div className="grid grid-cols-12 gap-2">
+//         <div className="col-span-3">
+//           <SideBar />
+//         </div>
+//         <div className="col-span-9">
+//           <div className="h-[8%]">
+//             <div className="float-right mr-2 mt-2">
+//               <ModeToggle />
+//             </div>
+//           </div>
+//           {children}
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
+import { SideBar } from "./sideBar"
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+import { Separator } from "@/components/ui/separator"
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar"
+
+export default function Page() {
   return (
-    <>
-      <div className="grid grid-cols-12 gap-2">
-        <div className="col-span-3">
-          <SideBar />
-        </div>
-        <div className="col-span-9">
-          <div className="h-[8%]">
-            <div className="float-right mr-2 mt-2">
-              <ModeToggle />
-            </div>
+    <SidebarProvider>
+      <SideBar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+          <div className="flex items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator orientation="vertical" className="mr-2 h-4" />
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem className="hidden md:block">
+                  <BreadcrumbLink href="#">
+                    Building Your Application
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator className="hidden md:block" />
+                <BreadcrumbItem>
+                  <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
           </div>
-          {children}
+        </header>
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+          <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+            <div className="aspect-video rounded-xl bg-muted/50" />
+            <div className="aspect-video rounded-xl bg-muted/50" />
+            <div className="aspect-video rounded-xl bg-muted/50" />
+          </div>
+          <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
         </div>
-      </div>
-    </>
-  );
+      </SidebarInset>
+    </SidebarProvider>
+  )
 }
